fc1857e539c3c32b9d35f2161487a19bf0349027
Moonsolos <1056860@hr.nl>
2023-10-02 19:22:38 +0200

    added some routes

--- database/migrations/2023_09_18_105550_create_blogs_table.php
+++ database/migrations/2023_09_18_105550_create_blogs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title', 100);
             $table->text('description');
-            $table->float('price');
+            $table->string('tier');
             $table->timestamps();
         });
     }
